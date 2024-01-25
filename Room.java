aba69efbf16d25a26975a4404f8361819b4a8907
desmondj2atwit <desmondj2@wit.edu>
2024-01-25 11:43:18 -0500

    Room class

--- Room.java
+++ Room.java
@@ -1,1 +1,18 @@
+public class Room{
+	public String name;
+	public boolean visited;
+	public Object[] objects;
+	public Room[] connections;
+	
+	public Room(Sting name, Object[] obj){
+		this.name = name;
+		this.visited = False;
+		this.objects = obj;
+		this.connections = [];
+	}
 
+	public static void connect(Room r){
+		this.connections.add(r);
+		r.connections.add(this);
+	}
+}
