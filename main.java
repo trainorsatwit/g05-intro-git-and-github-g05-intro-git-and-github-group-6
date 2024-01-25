9291713368785e36abb5c869cb326e4d7957c6c8
pateln19atwit <122486352+pateln19atwit@users.noreply.github.com>
2024-01-25 11:42:25 -0500

    Update main.java
    did the basic decision making

--- main.java
+++ main.java
@@ -1,5 +1,29 @@
-public class main {
-    System.out.println("PARAGRAPH EXPLAINING THE GAME AND RULES");
+import java.util.Scanner;
 
-    rooms = ["Foyer", "Living Room", "Kitchen", "Bedroom 1", "Bedroom 2", "Bathroom"]
+public class main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    
+        System.out.println("PARAGRAPH EXPLAINING THE GAME AND RULES");
+
+        boolean keyFound = false;
+        int x = 1;
+        while (true) {
+            System.out.println(x);
+            String input = scan.nextLine();
+            System.out.println(input);
+            if (input.equals("move")) {
+                System.out.println("MOVE TO...");
+            }
+            else if (input.equals("search")) {
+                System.out.println("SEARCHING...");
+            }
+            else if (input.equals("fail")) {
+                System.out.println("YOU GAVE UP :(");
+                break;
+            }
+            x++;
+        }
+    }
 }
