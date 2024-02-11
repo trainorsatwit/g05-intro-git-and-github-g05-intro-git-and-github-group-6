b00892daa9575bb78dfda81dcabbee85f434e5a4
trainorsatwit <71520869+trainorsatwit@users.noreply.github.com>
2024-02-11 16:46:40 -0500

    Update Furniture.java

--- Furniture.java
+++ Furniture.java
@@ -1,10 +1,15 @@
-public class Furniture {
-    private boolean hasKey;
+public class Furniture 
+{
+    private boolean hasKey1;
+    private boolean hasKey2;
+    private boolean hasKey3;
     private boolean hasBeenLookedAt = false;
     private final String name;
-    public Furniture(boolean key, String objName)
+    public Furniture(boolean keyslot1, boolean keyslot2, boolean keyslot3, String objName)
     {
-        hasKey = key;
+        hasKey1 = keyslot1;
+        hasKey2 = keyslot2;
+        hasKey3 = keyslot3;
         name = objName;
     }
     public String getName() {
@@ -16,8 +21,23 @@ public class Furniture {
     public void setLook(boolean update){
         hasBeenLookedAt = update;
     }
-    public boolean hasKey(){
-        return hasKey;
+    public boolean hasKey(int slot){
+    	if(slot==1)
+        	return hasKey1;
+        else if(slot==2)
+        	return hasKey2;
+        else if(slot==3)
+        	return hasKey3;
+        else
+        	return false;
     }
-    public void setHasKey(boolean update) {hasKey = update;}
+    public void setHasKey(int slot) {
+    if(slot==1)
+    	hasKey1 = true;
+    else if(slot==2)
+    	hasKey2 = true;
+    else
+    	hasKey3 = true;
+    }
 }
+
